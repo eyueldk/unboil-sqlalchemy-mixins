5519ce3325ce82af87429ba95527dc7f2c21604a
eyueldk <eyueldk@gmail.com>
2025-08-14 11:33:00 +0300

    Bump version to 0.1.8 and include UserOwnedMixin in module exports

--- src/unboil_sqlalchemy_mixins/__init__.py
+++ src/unboil_sqlalchemy_mixins/__init__.py
@@ -1,9 +1,11 @@
 from .identifiable import IdentifiableMixin
 from .timestamped import TimestampedMixin
 from .tenant_owned import TenantOwnedMixin
+from .user_owned import UserOwnedMixin
 
 __all__ = [
     "IdentifiableMixin", 
     "TimestampedMixin",
-    "TenantOwnedMixin"
+    "TenantOwnedMixin",
+    "UserOwnedMixin",
 ]
